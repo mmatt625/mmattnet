43821c1659869f0bc7d942d8d4267d59dcb266b1
matt <30363562+mmatt625@users.noreply.github.com>
2019-06-13 11:31:35 -0500

    nolitebans

--- litebans-php-master/inc/footer.php
+++
@@ -1,3 +0,0 @@
-<script src="<?php echo $this->autoversion('inc/js/jquery-3.3.1.min.js'); ?>"></script>
-<script src="<?php echo $this->autoversion('inc/js/bootstrap.min.js'); ?>"></script>
-<?php echo "</html>"; ?>
